c899bb68c9590ca209f012cc21cfe5f7ff4aef6f
chengchen <chengchen216@gmail.com>
2015-05-21 21:51:16 +0800

    remove mysql.dll

--- EDMWebsite/Controllers/TestController.cs
+++ EDMWebsite/Controllers/TestController.cs
@@ -6,7 +6,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using MySql.Data.Entity;
+//using MySql.Data.Entity;
 
 namespace EDMWebsite.Controllers
 {
--- EDMWebsite/Models/Context/IdentityModels.cs
+++ EDMWebsite/Models/Context/IdentityModels.cs
@@ -3,7 +3,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-using MySql.Data.Entity;
+//using MySql.Data.Entity;
 
 namespace EDMWebsite.Models
 {
